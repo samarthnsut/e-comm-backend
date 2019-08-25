8cd95296707710e2e3241beef3cc67b055be8e83
samarth <45564142+samarthnsut@users.noreply.github.com>
2019-08-25 22:57:59 +0530

    Create moongose.js

---
+++ config/moongose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/e_commerce_account_db', {UseNewUrlParser: true});
+
+var db = mongoose.connection;
+
+db.on('error',console.error.bind("error in connecting to db"));
+
+db.once('open',function(){
+    console.log("database connection successful");
+})
+
