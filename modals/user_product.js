e4298e4bd4da5c5db4c1a1e6848d786bc2c3a09c
samarth <45564142+samarthnsut@users.noreply.github.com>
2019-10-09 00:11:57 +0530

    a

---
+++ modals/user_product.js
@@ -0,0 +1,19 @@
+const mongoose= require("mongoose")
+
+const productSchema= new mongoose.Schema({
+    pname :{
+        type : String,
+        require : true
+    },
+    description:{
+        type : String
+    },
+    account :{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "Account"
+    }
+},{
+    timestamps: true
+})
+const Product= mongoose.model("Product",productSchema);
+module.exports= Product;
